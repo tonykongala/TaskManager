e8c1d14ab9823970a8b46f780a647fb8d2022c8f
tonykongala <tonylamington.kongala@gmail.com>
2025-04-30 13:25:57 -0500

    Update index.jsx

--- frontend/src/api/index.jsx
+++ frontend/src/api/index.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_API_URL || "https://backend-zpc2.onrender.com",
   headers: {
     "Content-Type": "application/json"
   }
