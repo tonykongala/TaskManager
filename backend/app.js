d0199887c1d8fd82c07aa11a152cfa3263955210
tonykongala <tonylamington.kongala@gmail.com>
2025-04-30 06:11:29 -0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -29,10 +29,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/profile", profileRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(__dirname, "../frontend/build")));
-  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../frontend/build/index.html")));
-}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
