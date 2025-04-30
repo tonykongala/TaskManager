629c83fe56d4958afb3d5ddabac38eadf01976d5
tonykongala <tonylamington.kongala@gmail.com>
2025-04-30 12:50:25 -0500

    removed static frontend

--- backend/app.js
+++ backend/app.js
@@ -38,14 +38,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/profile", profileRoutes);
 
-// ======== Static Assets for Production ========
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(__dirname, "../frontend/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-  });
-}
-
 // ======== Start Server ========
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
